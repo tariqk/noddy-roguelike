b8b3c3ddc7166d7215e10d02d749629fa3cb83ad
Tariq Kamal <github.t-boy@xoxy.net>
2015-01-07 22:28:26 +0800

    Barely any basics up, and tweaks to protag icon.
    Going through Round Two of @kcunning's entry:
    
    http://therealkatie.net/blog/2012/feb/13/pygame-getting-started/
    
    Breaking it up and basically constructed an empty level for now. Ran a
    test to see if the thing would compile in Emacs. Finally managed it to
    some degree. Slow start. Ugh.
    
    Also, tweaked the protagonist icon for better symmetry. Small
    distractions. #welp

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python
+
+import pygame, math, sys
+from pygame.locals import *
+
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+MAPSIZE = (20,15)
+
+class Level(object):
+    """
+    Represents the current level.
+    """
+    def __init__(self):
+        self.map = [] # right now it's empty; later we'll load or generate our own
+        for spam in range(MAPSIZE[0]):
+            row = []
+            for eggs in range (MAPSIZE[1]):
+                row.append(0) # assumption: 0 is empty floor?
+            self.map.append(row)
+
+    def print_ascii_map(self):
+        for row in self.map:
+            print row
+
+class Game(object):
+    def __init__(self):
+        self.current_level = Level()
+        self.current_level.print_ascii_map()
+
+def main():
+        game = Game()
+
+if __name__ == "__main__":
+    main()
