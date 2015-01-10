2baaf2bd8bc1af45d1bdb1ddba2df1934877c5ca
Tariq Kamal <github.t-boy@xoxy.net>
2015-01-10 11:39:45 +0800

    So much time taken for a black triangle moment LOL
    Seriously, though, damn sad. How many days since the last commit? I've
    been basically stealing as much time and fending off the toddler while
    doing this XD
    
    Anyway, managed to draw a black window of the required size (right now
    hard-coded) and draw absolutely nothing. It'll quit after I press
    escape.
    
    LOL. Sad kan?
    
    Anyway.

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 
-import pygame, math, sys
+import pygame, math, sys, os
 from pygame.locals import *
+
+# CONSTANTS
+
+# colors
 
 BLACK = (0,0,0)
 WHITE = (255, 255, 255)
-MAPSIZE = (20,15)
+
+# sizes
+
+MAPSIZE = (20,15) # placeholder; decision to be made -- dynamic map sizes or fixed?
+TILESIZE = 16 # ASSUMPTION: icons are square
+WINDOW_SIZE = (320,240) # placeholder
+
+# resource locations
+
+RES_DIR = os.getcwd() + '/noddy/res/'
+
+# CLASSES
 
 class Level(object):
     """
@@ -13,7 +28,7 @@ class Level(object):
     """
     def __init__(self):
         self.map = [] # right now it's empty; later we'll load or generate our own
-        for spam in range(MAPSIZE[0]):
+        for spam in range(MAPSIZE[0]): # fill everything with zeroes.
             row = []
             for eggs in range (MAPSIZE[1]):
                 row.append(0) # assumption: 0 is empty floor?
@@ -24,12 +39,36 @@ class Level(object):
             print row
 
 class Game(object):
+    """
+    Represents the current game display, right now.
+    """
     def __init__(self):
         self.current_level = Level()
-        self.current_level.print_ascii_map()
+
+        self.clock = pygame.time.Clock()
+        self.display = pygame.display.set_mode(WINDOW_SIZE)
+        
+        self.run()
+
+    def run(self):
+        """
+        Right now, just running things and listens to key presses for the exit key.
+        """
+        while 1:
+            self.clock.tick(30)
+            for event in pygame.event.get(): # potential improvement -- use dict?
+                if not hasattr(event, 'key'): continue
+                if event.key == K_ESCAPE: pygame.quit()
+
+# UTILITY FUNCTIONS
 
 def main():
-        game = Game()
+    """ 
+    main loop 
+    """
+    game = Game()
 
+# THE RUNNING PROGRAM
+        
 if __name__ == "__main__":
     main()
